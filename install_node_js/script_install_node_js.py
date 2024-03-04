f7439dbae2513e3c0c0c519459845f783b09b782
felix sindani <felixsindani72@gmail.com>
2024-03-04 14:43:40 +0000

    uploaded nodejs installer script

---
+++ install_node_js/script_install_node_js.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#Script Install NodeJS 12.11.x
+#(in your home directory):
+
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+
+## You may also need development tools to build native addons:
+#sudo apt-get install gcc g++ make
+
+nodejs -v
+npm -v
+
+#install Jest, Babel and ESList by using the supplied conf files and run npm install
+vi package.json
+vi babel.config.js
+vi vi .eslintrc.js
+
+npm install
