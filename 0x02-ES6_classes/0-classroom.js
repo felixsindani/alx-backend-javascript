cb4470dd8e957cce2dc24bc11a97bf92c59a9411
felixsindani <felixsindani72@gmail.com>
2024-03-07 23:53:51 +0300

    tasks

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Implement a class named ClassRoom
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The max number of students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
